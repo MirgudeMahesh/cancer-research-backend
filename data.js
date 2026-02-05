66fa2bb8b2d84f30370f38eb431b2689a926ef32
MirgudeMahesh <mirgudemahesh2002@gmail.com>
2026-02-05 13:37:29 +0530

    email error

--- data.js
+++ data.js
@@ -104,11 +104,16 @@ pool
 // ---------- Nodemailer Setup ----------
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000, // 10 seconds
+    greetingTimeout: 5000,
+    socketTimeout: 30000
 });
 
 // ---------- Health check ----------
